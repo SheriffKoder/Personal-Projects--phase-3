26efb75becb9cc8334a3f1291fd821f8fd146153
SheriffKoder <sheriff.koder@hotmail.com>
2023-09-13 21:39:36 +0300

    Setup react and libraries, introduction to styled components

---
+++ projects/react-movie-app/README.js
@@ -0,0 +1,100 @@
+
+//////////////////////////////////////
+//Setup
+/*
+
+//will use react-router-6
+history is another library that will be used with react-router
+
+# npm install history react-router-dom@next
+
+
+//styled-components library
+to be able to write css in isolated and scoped components
+
+# npm i styled-components
+
+//prop-types library
+to type check the props sent into components
+
+# npm install --save prop-types
+
+
+
+API.js: functions that will handle the api calls to the movie database api
+(in the previous course)
+
+config.js: everything that should be setup with the database api
+helpers.js: function to convert numbers-to-money/time
+
+
+//create a .env file in the project root
+react has built in support for env variables
+but have to name them REACT_NAME
+env is not the safest way to store variables, as the keys can still be visible on the client browser ?
+
+
+
+
+
+*/
+
+
+
+//////////////////////////////////////
+//Introduction
+/*
+
+app.js is the heart of our application
+index.js is the start of our application
+
+React.StrictMode, do checks if did some things when writing your application
+
+JSX, javascript xml
+
+use state in a parent component, if you want to use that state
+in multiple child components
+
+should never change the prop value in the child component that inherits the props
+only change from the parent/source
+but can change the state with a setter in the child component
+
+html element text
+{switchOnProp ? "On" : "Off"}
+
+
+
+
+whats new ?
+- createElement instead of returning JSX directly
+- styled components
+
+
+*/
+
+
+//hour 2
+//////////////////////////////////////
+//Introduction
+/*
+
+create a new file in the src folder GlobalStyle.js
+
+where we import the globalStyle from style-components
+and these global style components will be used in the App.js
+
+!cant change the properties of a const object, but can change the object itself
+
+
+
+
+what's new ?
+- 
+-
+
+
+*/
+
+
+
+
---
+++ projects/react-movie-app/src/App.js
@@ -0,0 +1,78 @@
+import React from "react";
+import {useState} from "react";
+
+//another way of creating elements than JSX
+/*
+const Star = () => React.createElement("div", null, "This is a little star")
+
+const App = () => {
+  return Star();
+}
+*/
+
+
+
+
+//uplifting state (Lamp switch example)
+/*
+import styled from "styled-components";
+import Lamp from "./components-lightSwitch/Lamp";
+import LightSwitch from "./components-lightSwitch/LightSwitch";
+
+//styled "Component"
+const Room = styled.div`
+position: relative;
+width: 500px;
+height: 500px;
+border: 10px solid black;
+margin: 0 auto
+`;
+
+//only use the setState to change the state, in order to cause a re-render
+//setters will be called with the previous state
+//will use the lamp state on both the lamp and lightSwitch
+function App () {
+  const [isLampOneOn, setIsLampOneOn] = useState(false);
+  const [isLampTwoOn, setIsLampTwoOn] = useState(true);
+
+  const handleLightSwitchOne = () => setIsLampOneOn(prev => !prev);
+  const handleLightSwitchTwo = () => setIsLampTwoOn(prev => !prev);
+
+  return (
+    <Room>
+      <Lamp lampOn={isLampOneOn} position="left" />
+      <Lamp lampOn={isLampTwoOn} position="right" />
+
+      <LightSwitch
+        name="one"
+        callback={handleLightSwitchOne}
+        switchOn={isLampOneOn}
+        position="left"
+      />
+
+      <LightSwitch
+        name="two"
+        callback={handleLightSwitchTwo}
+        switchOn={isLampTwoOn}
+        position="right"
+      />
+
+
+    </Room>
+  );
+
+
+};
+*/
+
+
+
+const App = () => {
+  return (
+    <div className="App">
+      Start here.
+    </div>
+  );
+}
+
+export default App;
