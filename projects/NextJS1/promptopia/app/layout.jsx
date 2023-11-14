156a02ef304b59703aee0bff9c8d09b98e7b5877
SheriffKoder <sheriff.koder@hotmail.com>
2023-11-14 03:33:15 +0200

    Work on the file structure, site layout and the navigation component

--- projects/NextJS1/promptopia/app/layout.jsx
+++ projects/NextJS1/promptopia/app/layout.jsx
@@ -1,18 +1,21 @@
 // import React from 'react'
 
 
-//import styles
+//import styles //01
 import '@styles/globals.css';   //import the css to the entire application
 
+//01.02
+import { Nav } from '@components/Nav';
+import { Provider } from '@components/Provider';
 
-//define metadata
+//define metadata //01
 export const metadata = {
     title: "Promptopia",
     description: "Discover & Share AI Prompts"
 }
 
-//define layout component
-const RootLayout = () => {
+//define layout component //01
+const RootLayout = ({ children }) => {
     return (
         <html lang="en">
             <body>
@@ -23,6 +26,8 @@ const RootLayout = () => {
                 </div>
 
                 <main className="app">
+                    <Nav />
+                    {/* this will hold the page.jsx */}
                     {children}
                 </main>
 
@@ -32,4 +37,5 @@ const RootLayout = () => {
 }
 
 
+//export layout component //01
 export default RootLayout;
