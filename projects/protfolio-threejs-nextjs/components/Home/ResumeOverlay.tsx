11339cecc8ad8dc126b8cfbaee0435e55c462fff
SheriffKoder <102818968+SheriffKoder@users.noreply.github.com>
2024-06-25 01:28:15 +0300

    More @hero resume overlay styling

--- projects/protfolio-threejs-nextjs/components/Home/ResumeOverlay.tsx
+++ projects/protfolio-threejs-nextjs/components/Home/ResumeOverlay.tsx
@@ -25,37 +25,38 @@ const ResumeOverlay = () => {
     id="resumeOverlayContainer"
     className="fixed h-[100vh] w-[100vw] z-[99] hidden items-center justify-center 
     overflow-hidden bg-[rgba(2,8,18,0.79)]">
-        <div className="w-[100vw] h-[1px]
-        hidden flex-col items-center justify-center gap-8 overflow-hidden"
+        <div className="w-[100vw] h-[0px]
+        hidden flex-col items-center justify-center  overflow-hidden"
         id="resumeOverlay">
         
-            <div className="flex flex-row gap-4">
-                <div className="px-4 py-1 font-base
-                gradientGreyButton focus:opacity-95 hover:opacity-95
-                text-[min(0.9em,1rem)]">
-                    <a href="./documents/Resume-sherif-khodeir-prof-2024.pdf" download="Resume-sherif-khodeir-prof-2024.pdf"
-                    className="text-white flex flex-row gap-2">
-                    Simple version
-                    <span className="mt-[2px]">
-                        <DownloadIcon color="#ffff"/>
-                    </span>
-                    </a>
+            <div className="w-[400px] flex flex-col items-center justify-center gap-8 relative">
+                <div className="flex flex-row gap-4">
+                    <div className="px-4 py-1 font-base
+                    gradientGreyButton focus:opacity-95 hover:opacity-95
+                    text-[min(0.9em,1rem)]">
+                        <a href="./documents/Resume-sherif-khodeir-prof-2024.pdf" download="Resume-sherif-khodeir-prof-2024.pdf"
+                        className="text-white flex flex-row gap-2">
+                        Simple version
+                        <span className="mt-[2px]">
+                            <DownloadIcon color="#ffff"/>
+                        </span>
+                        </a>
+                    </div>
+                    <div className="px-4 py-1 font-base
+                    gradientGreyButton focus:opacity-95 hover:opacity-95
+                    text-[min(0.9em,1rem)]">
+                        <a href="./documents/Resume-sherif-khodeir-grph-2024.pdf" download="Resume-sherif-khodeir-grph-2024.pdf"
+                        className="gradient_text_1 flex flex-row gap-2">
+                        Colored version
+                        <span className="mt-[2px]">
+                            <DownloadIcon color="#39D0B7"/>
+                        </span>
+                        </a>
+                    </div>
                 </div>
-                <div className="px-4 py-1 font-base
-                gradientGreyButton focus:opacity-95 hover:opacity-95
-                text-[min(0.9em,1rem)]">
-                    <a href="./documents/Resume-sherif-khodeir-grph-2024.pdf" download="Resume-sherif-khodeir-grph-2024.pdf"
-                    className="gradient_text_1 flex flex-row gap-2">
-                    Colored version
-                    <span className="mt-[2px]">
-                        <DownloadIcon color="#39D0B7"/>
-                    </span>
-                    </a>
+                <div className="absolute bottom-[-2rem] cursor-pointer" onClick={hideResumeOverlay}>
+                    close
                 </div>
-            </div>
-
-            <div className="" onClick={hideResumeOverlay}>
-                close
             </div>
         </div>
         
