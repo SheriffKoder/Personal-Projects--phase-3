43e7abdb014a79ed195fd42d29081c93664a4cde
SheriffKoder <102818968+SheriffKoder@users.noreply.github.com>
2024-11-11 21:01:46 +0200

    update the image slider at recommended propeties section

--- projects/re-nextjs/components/Home/Home_Rec.tsx
+++ projects/re-nextjs/components/Home/Home_Rec.tsx
@@ -14,67 +14,45 @@ import Link from "next/link";
 
 const Home_Rec = () => {
 
+    // remove the class after a slight delay to avoid remove/add overlap
+    function FadeControl (element:Element) {
+        element.classList.add("fade");
 
-    ////////////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////////////
+        setTimeout(()=>{
+            element.classList.remove("fade");
 
-    function fadeOutAnimation (slider__container: any) {
-        //[fadeOut]
-       slider__container?.classList.add("fadeOut_animation");
-        
+        },1000);
+
     }
 
-    function delayAnimation (slider__container: any){
-        //give [delay], after fadeOut(600ms) finishes
-        setTimeout(()=> {
-            handleSliderIndex(fade);
+    function changeImageForward () {
+        let slider = document.querySelector(".LunSlider");
 
-        },600);
-    }
+        if (slider) {
+          let img = slider.querySelector("a:first-child");
+          if (img) {
+            slider.append(img);
 
-    function handleSliderIndex (direction: number) {
-
-        if (direction < prevFade) {
-            // console.log("left");
-            if (propertiesRec !== null)
-            (sliderIndex > 0) ? setSliderIndex(sliderIndex-1) : setSliderIndex(propertiesRec.length-1);
-            setPrevFade(fade);
-        } else if (direction > prevFade) {
-            // console.log("right");
-            if (propertiesRec !== null)
-            (sliderIndex < propertiesRec.length-1) ? setSliderIndex(sliderIndex+1) : setSliderIndex(0);
+            // add fade animation to the content that will be displayed
+            let currentImg = slider.querySelector("a:first-child");
+            if (currentImg) FadeControl(currentImg);
 
         }
+        }
+      }
+        
+      function changeImageBackward () {
+        let slider = document.querySelector(".LunSlider");
+        if (slider) {
+          let img = slider.querySelector("a:last-child");
+          if (img) {slider.prepend(img); };
+        }
+      }
 
-    }
-
-    function fadeInAnimation (slider__container: any) {
-
-        //[fadeIn] after fadeout(600ms) and delay(200ms) finish
-        setTimeout(()=> {
-            slider__container?.classList.add("fadeIn_animation");
-        },800);
-
-        //after fadeOut,delay,fadeIn finish, remove to be re-applied
-        setTimeout(()=> {
-            slider__container?.classList.remove("fadeOut_animation");
-            slider__container?.classList.remove("fadeIn_animation");
-        },1400);
-
-
-
-
-    }
-
-    function animationCombination (slider__container: any) {
+      function animationCombination () {
 
         // //stop current timers to not overlap
         stopTimer();
-
-        fadeOutAnimation(slider__container);
-        delayAnimation(slider__container); //with handle fade change
-        
-        fadeInAnimation(slider__container);
 
         //start the auto animation timer after first render or again after caret click
         startTimer();
@@ -90,61 +68,62 @@ const Home_Rec = () => {
     function startTimer () {
         tm.current = window.setInterval(() => {
             // console.log("timer");
-            setPrevFade(fade); setFade(fade+1);
-        }, 6000);
+            changeImageForward();
+        }, 3000);
     }
 
-    ////////////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////////////
-
-
-    const [prevFade, setPrevFade] = useState(0);
-    const [fade, setFade] = useState(0);
-    const [sliderIndex, setSliderIndex] = useState(0);
     
-    const tm = useRef(0);
 
     //04.01
     const [propertiesRec, setPropertiesRec] = useState<PropertyDocument[] | []>([]);
+    // const [sliderIndex, setSliderIndex] = useState(0);
+
+    const tm = useRef(0);
+    const forwardButton = useRef(null);
+    const backButton = useRef(null);
 
 
+    // fetch recommended properties on component load
     useEffect(()=> {
         
         let slider__container = document.querySelector("#slider__container"); 
 
         const fetchProperties = async () => {
-        const responseRec = await fetch("/api/properties/homePage_rec");
-        const jsonResponseRec = await responseRec.json();
+            const responseRec = await fetch("/api/properties/homePage_rec");
+            const jsonResponseRec = await responseRec.json();
 
 
-        //filter out un-used images slots in every property in the json response
-        jsonResponseRec.forEach((property: PropertyDocument) => {
-            property.property_images = property.property_images.filter((image: string) => {
-                if (image !== "") {
-                return image;
-                }
-            });
-        })
+            //filter out un-used images slots in every property in the json response
+            jsonResponseRec.forEach((property: PropertyDocument) => {
+                property.property_images = property.property_images.filter((image: string) => {
+                    if (image !== "") {
+                    return image;
+                    }
+                });
+            })
 
-        console.log("Rec Properties");
-        console.log(jsonResponseRec);
-        setPropertiesRec(jsonResponseRec);
+            console.log("Rec Properties");
+            console.log(jsonResponseRec);
+            setPropertiesRec(jsonResponseRec);
         
-
         }
 
         //as the useEffect triggers on first load AND fade state (set by timer or buttons)
         //we do not want to fetch properties again if there are
         //if there are properties, just set the timer to animate and change the property index
-        if (propertiesRec.length < 1) fetchProperties();  animationCombination(slider__container);   
+        if (propertiesRec.length < 1) {
+            fetchProperties();  
+            animationCombination();
+        }
 
         if (propertiesRec.length > 1) {
-            animationCombination(slider__container);    
+            animationCombination();    
         } 
 
 
 
-    },[fade]);
+    // },[fade]);
+    },[]);
 
 
   return (
@@ -160,55 +139,49 @@ const Home_Rec = () => {
         </h2>
 
         {/* arrows and image with info */}
-        <div className="py-12 relative">
+        <div className="py-12 mt-6 relative flex flex-row gap-4 h-[calc(50vw)] 
+        max-h-[500px] items-center justify-between w-[85%] max-w-[1250px] mx-auto">
             
             {/* <div className="absolute w-full h-full rounded-b-[10px] top-0 left-0 z-[0] opacity-[0.03]"
             style={{backgroundImage: "url('/images/deco.png')", backgroundSize: "", backgroundPosition: "-10px -40px" }}>
 
             </div> */}
 
-
-            <div className="flex flex-row items-center justify-center gap-2 md:gap-8 h-[50vw] max-h-[500px]">
+            <div className="absolute z-[0] h-[50vw] max-h-[500px] w-full
+            flex overflow-hidden rounded-[5px] LunSlider"
+            onMouseEnter={stopTimer} onMouseLeave={animationCombination}>
             
-                {/* prevFade to determine direction in the animation function */}
-                <button
-                    onClick={()=>{setPrevFade(fade); setFade(fade-1);}}
-                    // onClick={()=>{prevFade=fade; fade=fade-1; animationCombination(slider__container);}}
-                    className="bg-[url('/icons/arrow-left.svg')] h-[3vw] w-[3vw] max-h-[40px] max-w-[40px]
-                    min-w-[20px] min-h-[20px] bg-no-repeat bg-contain">
-                </button>
-                {/* maintain 16:9 aspect ratio */}
-                {/* the image and the property summarized info */}
-                <Link className="rounded-[17px] border-0
-                w-full h-[calc(50vw)] max-h-[500px] max-w-[833px]
-                relative
+            {propertiesRec.map((prop, sliderIndex)=> (
+                <Link className="
+                
+                relative min-w-full
                 "
                 id="slider__container"
                 href={`/properties/single/${propertiesRec[sliderIndex]._id}`}>
                     {/* image */}
                     <Image src={propertiesRec[sliderIndex].property_images[0]} fill={true} priority alt={propertiesRec[sliderIndex].property_city}
-                    onMouseEnter={stopTimer} onMouseLeave={startTimer}
-                    className="rounded-[17px] border-0  hover:opacity-100 dark:opacity-75 dark:hover:opacity-90
-                    w-full h-[calc(50vw)] max-h-[500px] max-w-[833px]"
+                    // onMouseEnter={stopTimer} onMouseLeave={startTimer}
+                    className=" border-0  hover:opacity-95 dark:opacity-100 dark:hover:opacity-95
+                    w-[90vw] h-[calc(50vw)] max-h-[500px]"
                     style={{objectFit:'cover'}}></Image>
                     {/* property summarized info */}
                     <div className="  absolute bottom-0 left-0
                     w-full dark:bg-[#0000005d] bg-[#9090905d]  text-white box-shadow-1
-                    text-[min(calc(0.5rem+0.5vw),(1.25rem))] font-normal capitalize rounded-b-[17px]">
+                    text-[min(calc(0.5rem+0.5vw),(1.25rem))] font-normal capitalize rounded-b-[5px]">
                         <div className="relative w-full h-full p-3 md:p-[min(calc(1rem+0.5vw),(2rem))]">
                             {/* <div className="z-[1] relative">{propertiesRec[sliderIndex].property_type} in {propertiesRec[sliderIndex].property_city} for {propertiesRec[sliderIndex].property_listing_type} </div> */}
                             {/* <div className="z-[1] relative">{propertiesRec[sliderIndex].property_area}m<sup>2</sup>, {propertiesRec[sliderIndex].property_beds} beds, {propertiesRec[sliderIndex].property_baths} baths </div> */}
-            
-                            <div className="absolute w-full h-full rounded-b-[10px] top-0 left-0 opacity-20 z-[0]"
+
+                            <div className="absolute w-full h-full rounded-b-[5px] top-0 left-0 opacity-20 z-[0]"
                             style={{backgroundImage: "url('/images/deco.png')", backgroundSize: "", backgroundPosition: "0 -60px" }}>
                             </div>
-            
+
                             <div className="relative lg:flex lg:flex-row lg:align-center">
                                 <span className="rounded-full pr-2 py-[0.125rem] lg:py-0 h-[1rem] lg:h-[1.5rem] lg:flex flex-row justify-center bg-[#ffffffc1] dark:bg-[#252525] text-black dark:text-[#e0e0e0]">
                                     <span className="rounded-full px-2 py-[0.125rem] lg:pt-0 lg:pb-[0.125rem] lg:flex flex-col justify-center ml-[-1px] bg-[#ab2546] dark:bg-[#171717] text-white dark:text-white">{propertiesRec[sliderIndex].property_city}</span>
                                     <span className="pl-1 py-[0.125rem] lg:pt-0 lg:pb-[0.125rem] pr-1 lg:flex flex-col justify-center">{propertiesRec[sliderIndex].property_district}</span>
                                 </span>
-            
+
                                 <div className="mt-1 lg:mt-0 lg:flex flex-row justify-center lg:ml-2">
                                 <span className="rounded-full pr-2 h-[1rem] py-[0.125rem] lg:py-0 lg:h-[1.5rem] lg:flex flex-row justify-center bg-[#ffffffc1] dark:bg-[#252525af] text-black dark:text-[#e0e0e0]">
                                     <span className="rounded-full px-2 py-[0.125rem] lg:pt-0 lg:pb-[0.125rem] lg:flex flex-col justify-center ml-[-1px]  bg-[#ab2546] dark:bg-[#171717] text-white dark:text-white">{propertiesRec[sliderIndex].property_type}</span>
@@ -220,19 +193,42 @@ const Home_Rec = () => {
                                     </span>
                                 </div>
                             </div>
-            
-            
+
+
                         </div>
-            
+
                     </div>
                 </Link>
-                <button
-                    onClick={()=>{setPrevFade(fade); setFade(fade+1);}}
-                    // onClick={()=>{prevFade=fade; fade=fade+1; animationCombination(slider__container);}}
-                    className="bg-[url('/icons/arrow-right.svg')]                             h-[3vw] w-[3vw] max-h-[40px] max-w-[40px]
-                    min-w-[20px] min-h-[20px] bg-no-repeat bg-contain">
-                </button>
+            ))}
+
+
+                
             </div>
+
+
+                <button ref={backButton}
+                onClick={changeImageBackward}
+                onMouseEnter={stopTimer} onMouseLeave={animationCombination}
+                className="relative z-[1] ml-12 bg-[#0a0a0a7d]  rounded-[3px]
+                bg-[url('/icons/arrow-left.svg')] h-14 w-14 bg-no-repeat bg-contain">
+                </button>
+
+                
+                <button ref={forwardButton}
+                onClick={changeImageForward}
+                onMouseEnter={stopTimer} onMouseLeave={animationCombination}
+                className="relative z-[1] mr-12 bg-[#0a0a0a7d] rounded-[3px]
+                bg-[url('/icons/arrow-right.svg')] h-14 w-14 bg-no-repeat bg-contain">
+                </button>
+ 
+
+ 
+
+
+
+
+
+
         </div>
 
 
